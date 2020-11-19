47676a9707c534236c028b9d191763be709027d7
Johan Martin Aarstein <johan.aarstein@gmail.com>
2020-11-19 09:43:14 +0100

    ..

--- src/jp-includes/update/update-core.php
+++ src/jp-includes/update/update-core.php
@@ -100,7 +100,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       }
     } else {
       http_response_code(500);
-      echo 'Unknown Error';
+      echo 'Permission Denied';
       exit();
     }
 
