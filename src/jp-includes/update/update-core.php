f8db961be1f9e827dcbfbf4c0d12f5f43682b2d5
Johan Martin Aarstein <johan.aarstein@gmail.com>
2020-11-19 13:50:03 +0100

    ..

--- src/jp-includes/update/update-core.php
+++ src/jp-includes/update/update-core.php
@@ -65,42 +65,43 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       }
     }
 
-    if ($flag && $version !== get_siteInfo()[0]['version']) {
-      try {
-        $phar = new PharData($target);
-        $phar -> extractTo(APP_ROOT . '/', null, true);
-        unlink($target);
-        custom_copy(APP_ROOT . '/src', APP_ROOT);
-        rrmdir(APP_ROOT . '/src');
-      } catch (Exception $e) {
-        http_response_code(500);
-        if (!empty($e)) {
-          echo $e;
-        } else {
-          echo 'Unknown Error';
+    if ($version !== get_siteInfo()[0]['version']) {
+      if ($flag) {
+        try {
+          $phar = new PharData($target);
+          $phar -> extractTo(APP_ROOT . '/', null, true);
+          unlink($target);
+          custom_copy(APP_ROOT . '/src', APP_ROOT);
+          rrmdir(APP_ROOT . '/src');
+        } catch (Exception $e) {
+          http_response_code(500);
+          if (!empty($e)) {
+            echo $e;
+          } else {
+            echo 'Unknown Error';
+          }
+          $db -> close();
+          exit();
         }
-        $db -> close();
+
+        $update = $db -> query(
+          "UPDATE `siteInfo`
+          SET     `version`   = '$version',
+                  `created`   = Now();"
+        );
+        if (!$update) {
+          http_response_code(500);
+          if ($db -> error) {
+            echo '(' . $db -> errno . '): ' . $db -> error;
+          }
+          $db -> close();
+          exit();
+        }
+      } else {
+        http_response_code(500);
+        echo 'Permission Denied';
         exit();
       }
-
-      $update = $db -> query(
-        "UPDATE `siteInfo`
-        SET     `version`   = '$version',
-                `created`   = Now();"
-      );
-      if (!$update) {
-        http_response_code(500);
-        if ($db -> error) {
-          echo '(' . $db -> errno . '): ' . $db -> error;
-        }
-        $db -> close();
-        exit();
-      }
-
-    } else {
-      http_response_code(500);
-      echo 'Permission Denied';
-      exit();
     }
 
   } else {
