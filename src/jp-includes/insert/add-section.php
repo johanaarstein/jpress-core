346f776c89a030477b0bd7ca649d416756fc791e
Johan Martin Aarstein <johan.aarstein@gmail.com>
2020-12-21 11:04:52 +0100

    ..

--- src/jp-includes/insert/add-section.php
+++ src/jp-includes/insert/add-section.php
@@ -4,6 +4,7 @@ require __DIR__ . '/../app/variables.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   if (isset($_POST['new-section'])) {
     require_once APP_ROOT . '/jp-config/config.php';
+    require_once APP_ROOT . '/jp-includes/functions.php';
     include APP_ROOT . '/jp-includes/lang/lang.php';
 
     $lang = $_POST['content-lang'];
