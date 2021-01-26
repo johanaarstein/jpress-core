5181242ce94e29bc963009da9f4c9a3eaafee1a9
Johan Martin Aarstein <johan.aarstein@gmail.com>
2021-01-26 11:23:42 +0100

    ..

--- src/jp-includes/app/functions.php
+++ src/jp-includes/app/functions.php
@@ -596,13 +596,16 @@ function rrmdir($dir) {
 
 function isPageSpeed() {
   $output = false;
-  // $headers = apache_request_headers();
-  if ($_SERVER['SERVER_ADDR'] !== '::1') {
-    $headers = get_headers(BASE_URL);
-    if (isset($headers['X-Mod-Pagespeed']) || isset($headers['X-Page-Speed'])) {
-      $output = true;
-    }
+  $headers = apache_request_headers();
+  if (isset($headers['X-Mod-Pagespeed']) || isset($headers['X-Page-Speed'])) {
+    $output = true;
   }
+  // if ($_SERVER['SERVER_ADDR'] !== '::1') {
+  //   $headers = get_headers(BASE_URL);
+  //   if (isset($headers['X-Mod-Pagespeed']) || isset($headers['X-Page-Speed'])) {
+  //     $output = true;
+  //   }
+  // }
   return $output;
 }
 
