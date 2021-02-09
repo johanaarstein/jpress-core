de27ad4cdc08a8afdceca05ce064d782c3c7c9f2
Johan Martin Aarstein <johan.aarstein@gmail.com>
2021-02-09 10:43:14 +0100

    ..

--- src/jp-includes/app/login-query.php
+++ src/jp-includes/app/login-query.php
@@ -117,11 +117,11 @@ if (empty($username_err) && empty($password_err) && empty($bruteforce_err)) {
               $stmt -> bind_param('ss', $dateNo, $username);
               $stmt -> execute();
             }
-            if (isPageSpeed()) {
+            // if (isPageSpeed()) {
               header("Location: /?ModPagespeed=off");
-            } else {
+            // } else {
               header("Location: /");
-            }
+            // }
             $stmt -> close();
             $db -> close();
             exit();
